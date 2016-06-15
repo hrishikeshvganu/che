2ecd02c697e80d3cb817ad254d37bceb9a305061
Anton Korneta <akorneta@codenvy.com>
2016-06-15 19:29:39 +0300

    CHE-1146 Factory structural refactoring

--- wsmaster/che-core-api-factory/src/main/java/org/eclipse/che/api/factory/server/DtoConverter.java
+++ wsmaster/che-core-api-factory/src/main/java/org/eclipse/che/api/factory/server/DtoConverter.java
@@ -10,14 +10,19 @@
  *******************************************************************************/
 package org.eclipse.che.api.factory.server;
 
-import org.eclipse.che.api.core.ApiException;
 import org.eclipse.che.api.factory.shared.dto.FactoryDto;
+import org.eclipse.che.api.factory.shared.model.Factory;
+import org.eclipse.che.dto.server.DtoFactory;
 
 /**
- * Convert legacy factory parameter to new the latest format
+ * Helps to convert to DTOs related to factory.
  *
- * @author Alexander Garagatyi
+ * @author Anton Korneta
  */
-public interface LegacyConverter {
-    void convert(FactoryDto factory) throws ApiException;
+public final class DtoConverter {
+    public static FactoryDto asDto(Factory factory) {
+        return DtoFactory.newDto(FactoryDto.class);
+    }
+
+    private DtoConverter() {}
 }
